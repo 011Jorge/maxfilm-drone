d096c19d70a356656e4556d604906921a090e49e
011Jorge <vicentejorge187@gmail.com>
2023-07-08 21:03:21 -0300

    :construction: navbar created

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,8 +1,10 @@
-import { ContainerHome } from "./styles";
+import Navbar from "../../components/Navbar";
+import { ContainerHome } from "./HomeStyles";
 
 function Home() {
   return (
     <ContainerHome>
+      <Navbar />
       <h1>I am Home</h1>
     </ContainerHome>
   );
