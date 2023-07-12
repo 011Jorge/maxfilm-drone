5231cc08e8cdfd323b4f4ccdfcf1f18425bc8c2b
011Jorge <vicentejorge187@gmail.com>
2023-07-11 22:18:11 -0300

    :construction: created new page

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import { GlobalStyle } from "./styles/globalStyles.js";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <GlobalStyle />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
